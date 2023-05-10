27c15222aff6d8503ccd498d55e033b022422d8b
Chani Wolpo <chani.wolpo@gmail.com>
2023-05-10 09:56:26 +0300

    first commit

---
+++ sandBox/calc.js
@@ -0,0 +1,8 @@
+const calc = (a, b, c) => {
+  switch (c) {
+    case "+":
+      return a + b;
+  }
+};
+
+module.exports = calc;
---
+++ sandBox/lastName.js
@@ -0,0 +1,4 @@
+const lastName =
+    "Wolpo"
+
+module.exports= lastName
